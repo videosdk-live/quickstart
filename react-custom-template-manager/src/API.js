19582cce545398a707abd94e101a0d48b6843164
Rajan Surani <rajansurani14@gmail.com>
2023-04-20 16:51:20 +0530

    tempalte manager added

---
+++ react-custom-template-manager/src/API.js
@@ -0,0 +1,16 @@
+export const authToken = "GENERTED TOKEN HERE";
+
+// API call to create meeting
+export const createMeeting = async ({ token }) => {
+  const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+    method: "POST",
+    headers: {
+      authorization: `${authToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({}),
+  });
+
+  const { roomId } = await res.json();
+  return roomId;
+};
