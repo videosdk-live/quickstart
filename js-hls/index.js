fb17733115a80ecc09cb305fccca4258450ba3e9
Tirth Shingala <teerthshingala08@gmail.com>
2023-06-05 18:36:30 +0530

    JS HLS QuickStart

---
+++ js-hls/index.js
@@ -0,0 +1,295 @@
+// getting Elements from Dom
+const joinHostButton = document.getElementById("joinHostBtn");
+const joinViewerButton = document.getElementById("joinViewerBtn");
+const leaveButton = document.getElementById("leaveBtn");
+const startHlsButton = document.getElementById("startHlsBtn");
+const stopHlsButton = document.getElementById("stopHlsBtn");
+const toggleMicButton = document.getElementById("toggleMicBtn");
+const toggleWebCamButton = document.getElementById("toggleWebCamBtn");
+const createButton = document.getElementById("createMeetingBtn");
+const videoContainer = document.getElementById("videoContainer");
+const textDiv = document.getElementById("textDiv");
+const hlsStatusHeading = document.getElementById("hlsStatusHeading");
+
+// declare Variables
+let meeting = null;
+let meetingId = "";
+let isMicOn = false;
+let isWebCamOn = false;
+
+const Constants = VideoSDK.Constants;
+
+// Join Meeting As Host Button Event Listener
+joinHostButton.addEventListener("click", async () => {
+  document.getElementById("join-screen").style.display = "none";
+  textDiv.textContent = "Joining the meeting...";
+
+  roomId = document.getElementById("meetingIdTxt").value;
+  meetingId = roomId;
+
+  await initializeMeeting(Constants.modes.CONFERENCE);
+});
+
+// Join Meeting As Viewer Button Event Listener
+joinViewerButton.addEventListener("click", async () => {
+  document.getElementById("join-screen").style.display = "none";
+  textDiv.textContent = "Joining the meeting...";
+
+  roomId = document.getElementById("meetingIdTxt").value;
+  meetingId = roomId;
+
+  await initializeMeeting(Constants.modes.VIEWER);
+});
+
+// Create Meeting Button Event Listener
+createButton.addEventListener("click", async () => {
+  document.getElementById("join-screen").style.display = "none";
+  textDiv.textContent = "Please wait, we are joining the meeting";
+
+  const url = `https://api.videosdk.live/v2/rooms`;
+  const options = {
+    method: "POST",
+    headers: { Authorization: TOKEN, "Content-Type": "application/json" },
+  };
+
+  const { roomId } = await fetch(url, options)
+    .then((response) => response.json())
+    .catch((error) => alert("error", error));
+  meetingId = roomId;
+
+  await initializeMeeting(Constants.modes.CONFERENCE);
+});
+
+// Initialize meeting
+async function initializeMeeting(mode) {
+  window.VideoSDK.config(TOKEN);
+
+  meeting = window.VideoSDK.initMeeting({
+    meetingId: meetingId, // required
+    name: "Tirth", // required
+    mode: mode,
+  });
+
+  meeting.join();
+
+  meeting.on("meeting-joined", () => {
+    textDiv.textContent = null;
+
+    document.getElementById("grid-screen").style.display = "block";
+    document.getElementById(
+      "meetingIdHeading"
+    ).textContent = `Meeting Id: ${meetingId}`;
+
+    if (meeting.hlsState === Constants.hlsEvents.HLS_STOPPED) {
+      hlsStatusHeading.textContent = "HLS Not Stared Yet";
+    } else {
+      hlsStatusHeading.textContent = `HLS Status: ${meeting.hlsState}`;
+    }
+
+    if (mode === Constants.modes.CONFERENCE) {
+      document.getElementById("speakerView").style.display = "block";
+    }
+  });
+
+  meeting.on("meeting-left", () => {
+    videoContainer.innerHTML = "";
+  });
+
+  meeting.on("hls-state-changed", (data) => {
+    const { status } = data;
+
+    hlsStatusHeading.textContent = `HLS Status: ${status}`;
+
+    if (mode === Constants.modes.VIEWER) {
+      if (status === Constants.hlsEvents.HLS_PLAYABLE) {
+        const { downstreamUrl } = data;
+        let video = document.createElement("video");
+        video.setAttribute("width", "100%");
+
+        if (Hls.isSupported()) {
+          var hls = new Hls();
+          hls.loadSource(downstreamUrl);
+          hls.attachMedia(video);
+          hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+          });
+        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+          video.src = downstreamUrl;
+          video.addEventListener("canplay", function () {
+            video.play();
+          });
+        }
+
+        videoContainer.appendChild(video);
+      }
+
+      if (status === Constants.hlsEvents.HLS_STOPPING) {
+        videoContainer.innerHTML = "";
+      }
+    }
+  });
+
+  if (mode === Constants.modes.CONFERENCE) {
+    // creating local participant
+    createLocalParticipant();
+
+    // setting local participant stream
+    meeting.localParticipant.on("stream-enabled", (stream) => {
+      setTrack(stream, null, meeting.localParticipant, true);
+    });
+
+    //  participant joined
+    meeting.on("participant-joined", (participant) => {
+      if (participant.mode === Constants.modes.CONFERENCE) {
+        participant.pin();
+
+        let videoElement = createVideoElement(
+          participant.id,
+          participant.displayName
+        );
+
+        participant.on("stream-enabled", (stream) => {
+          setTrack(stream, audioElement, participant, false);
+        });
+
+        let audioElement = createAudioElement(participant.id);
+        videoContainer.appendChild(videoElement);
+        videoContainer.appendChild(audioElement);
+      }
+    });
+
+    // participants left
+    meeting.on("participant-left", (participant) => {
+      let vElement = document.getElementById(`f-${participant.id}`);
+      vElement.remove(vElement);
+
+      let aElement = document.getElementById(`a-${participant.id}`);
+      aElement.remove(aElement);
+    });
+  }
+}
+
+// creating video element
+function createVideoElement(pId, name) {
+  let videoFrame = document.createElement("div");
+  videoFrame.setAttribute("id", `f-${pId}`);
+
+  //create video
+  let videoElement = document.createElement("video");
+  videoElement.classList.add("video-frame");
+  videoElement.setAttribute("id", `v-${pId}`);
+  videoElement.setAttribute("playsinline", true);
+  videoElement.setAttribute("width", "300");
+  videoFrame.appendChild(videoElement);
+
+  let displayName = document.createElement("div");
+  displayName.innerHTML = `Name : ${name}`;
+
+  videoFrame.appendChild(displayName);
+  return videoFrame;
+}
+
+// creating audio element
+function createAudioElement(pId) {
+  let audioElement = document.createElement("audio");
+  audioElement.setAttribute("autoPlay", "false");
+  audioElement.setAttribute("playsInline", "true");
+  audioElement.setAttribute("controls", "false");
+  audioElement.setAttribute("id", `a-${pId}`);
+  audioElement.style.display = "none";
+  return audioElement;
+}
+
+// creating local participant
+function createLocalParticipant() {
+  let localParticipant = createVideoElement(
+    meeting.localParticipant.id,
+    meeting.localParticipant.displayName
+  );
+  videoContainer.appendChild(localParticipant);
+}
+
+// setting media track
+function setTrack(stream, audioElement, participant, isLocal) {
+  if (stream.kind == "video") {
+    isWebCamOn = true;
+    const mediaStream = new MediaStream();
+    mediaStream.addTrack(stream.track);
+    let videoElm = document.getElementById(`v-${participant.id}`);
+    videoElm.srcObject = mediaStream;
+    videoElm
+      .play()
+      .catch((error) =>
+        console.error("videoElem.current.play() failed", error)
+      );
+  }
+  if (stream.kind == "audio") {
+    if (isLocal) {
+      isMicOn = true;
+    } else {
+      const mediaStream = new MediaStream();
+      mediaStream.addTrack(stream.track);
+      audioElement.srcObject = mediaStream;
+      audioElement
+        .play()
+        .catch((error) => console.error("audioElem.play() failed", error));
+    }
+  }
+}
+
+// leave Meeting Button Event Listener
+leaveButton.addEventListener("click", async () => {
+  meeting?.leave();
+  document.getElementById("grid-screen").style.display = "none";
+  document.getElementById("join-screen").style.display = "block";
+});
+
+// Toggle Mic Button Event Listener
+toggleMicButton.addEventListener("click", async () => {
+  if (isMicOn) {
+    // Disable Mic in Meeting
+    meeting?.muteMic();
+  } else {
+    // Enable Mic in Meeting
+    meeting?.unmuteMic();
+  }
+  isMicOn = !isMicOn;
+});
+
+// Toggle Web Cam Button Event Listener
+toggleWebCamButton.addEventListener("click", async () => {
+  if (isWebCamOn) {
+    // Disable Webcam in Meeting
+    meeting?.disableWebcam();
+
+    let vElement = document.getElementById(`f-${meeting.localParticipant.id}`);
+    vElement.style.display = "none";
+  } else {
+    // Enable Webcam in Meeting
+    meeting?.enableWebcam();
+
+    let vElement = document.getElementById(`f-${meeting.localParticipant.id}`);
+    vElement.style.display = "inline";
+  }
+  isWebCamOn = !isWebCamOn;
+});
+
+// Start Hls Button Event Listener
+startHlsButton.addEventListener("click", async () => {
+  meeting?.startHls({
+    layout: {
+      type: "SPOTLIGHT",
+      priority: "PIN",
+      gridSize: "20",
+    },
+    theme: "LIGHT",
+    mode: "video-and-audio",
+    quality: "high",
+    orientation: "landscape",
+  });
+});
+
+// Stop Hls Button Event Listener
+stopHlsButton.addEventListener("click", async () => {
+  meeting?.stopHls();
+});
