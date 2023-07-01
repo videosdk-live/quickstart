75c0bffb69d594af7acc9a168366d8c26808a86f
Yash Chudasama <yashchudasama1722@gmail.com>
2023-07-01 13:09:40 +0530

    Updated QuickStart for iOS SDK

--- ios-rtc/iOSQuickStartDemo/APIService/APIService.swift
+++ ios-rtc/iOSQuickStartDemo/APIService/APIService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 //Replace with the token you generated from VideoSDK Dashboard
-let TOKEN_STRING: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI0N2M3ZTJlYy01NzY5LTQ3OWQtYjdjNS0zYjU5MDcxYzhhMDkiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY3NjQ2NTgwMCwiZXhwIjoxODM0MjUzODAwfQ.b9JUWkfImLFpS85WABCmJALg6kDpFJe3JHfCTOVIZk4"
+let TOKEN_STRING: String = "<Token Generated From Videosdk Dashboard>"
 
 class APIService {
 
