336cc2a3ea203c88fbde7042b45dce4d0a7ac52c
harshitbagga <harshitbagga10@gmail.com>
2024-04-26 19:43:42 +0530

    Update environment file

--- Ionic-rtc-demo/src/environments/environment.ts
+++ Ionic-rtc-demo/src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJmMDJjOTJkNC0yOTBmLTRiM2UtOTViZi1lZTZkYTZmZTZhYWEiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcxNDAyNzAyMiwiZXhwIjoxNzE2NjE5MDIyfQ.kywAg-k66YFjUBD8HpdZ9u6QJkcPIxyJ0d4Ao6rsonY"
-
+  token: "YOUR_TOKEN"
 };
 
 /*
