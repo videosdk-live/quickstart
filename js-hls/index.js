557a41f2cc833437d50fcbb755315025437f8b80
TirthShingala <teerthshingala08@gmail.com>
2024-05-24 12:01:43 +0530

    js-hls hls config updated for smooth play

--- js-hls/index.js
+++ js-hls/index.js
@@ -70,16 +70,14 @@ function initializeMeeting(mode) {
 
         if (Hls.isSupported()) {
           var hls = new Hls({
-            maxLoadingDelay: 1, // max video loading delay used in automatic start level selection
+            maxLoadingDelay: 4, // max video loading delay used in automatic start level selection
             defaultAudioCodec: "mp4a.40.2", // default audio codec
-            maxBufferLength: 0, // If buffer length is/become less than this value, a new fragment will be loaded
-            maxMaxBufferLength: 1, // Hls.js will never exceed this value
             startLevel: 0, // Start playback at the lowest quality level
             startPosition: -1, // set -1 playback will start from intialtime = 0
             maxBufferHole: 0.001, // 'Maximum' inter-fragment buffer hole tolerance that hls.js can cope with when searching for the next fragment to load.
             highBufferWatchdogPeriod: 0, // if media element is expected to play and if currentTime has not moved for more than highBufferWatchdogPeriod and if there are more than maxBufferHole seconds buffered upfront, hls.js will jump buffer gaps, or try to nudge playhead to recover playback.
             nudgeOffset: 0.05, // In case playback continues to stall after first playhead nudging, currentTime will be nudged evenmore following nudgeOffset to try to restore playback. media.currentTime += (nb nudge retry -1)*nudgeOffset
-            nudgeMaxRetry: 1, // Max nb of nudge retries before hls.js raise a fatal BUFFER_STALLED_ERROR
+            nudgeMaxRetry: 3, // Max nb of nudge retries before hls.js raise a fatal BUFFER_STALLED_ERROR
             maxFragLookUpTolerance: 0.1, // This tolerance factor is used during fragment lookup.
             liveSyncDurationCount: 1, // if set to 3, playback will start from fragment N-3, N being the last fragment of the live playlist
             abrEwmaFastLive: 1, // Fast bitrate Exponential moving average half-life, used to compute average bitrate for Live streams.
