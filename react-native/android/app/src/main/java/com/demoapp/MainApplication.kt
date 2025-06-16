9652b83d97f589f9d250df6beea0c36521e37fd2
pavan901 <pavan@videosdk.live>
2025-06-16 18:18:23 +0530

    update quickstart react native

---
+++ react-native/android/app/src/main/java/com/demoapp/MainApplication.kt
@@ -0,0 +1,43 @@
+package com.demoapp
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import live.videosdk.rnwebrtc.WebRTCModulePackage
+
+class MainApplication : Application(), ReactApplication {
+
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> {
+          // Get the default list of packagesAdd commentMore actions
+          val packages = PackageList(this).packages.toMutableList()
+
+          // Add VideoSDK WebRTC package manually
+          packages.add(WebRTCModulePackage())
+
+          return packages
+        }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
+
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    loadReactNative(this)
+  }
+}
