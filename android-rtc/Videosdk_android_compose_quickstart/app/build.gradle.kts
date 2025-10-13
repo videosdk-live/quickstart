c097fd558cfae80eff3e8a6e79fbf121926058ec
HiDeepakk <deepak@videosdk.live>
2025-10-13 20:32:27 +0530

    chore: version upgraded.

--- android-rtc/Videosdk_android_compose_quickstart/app/build.gradle.kts
+++ android-rtc/Videosdk_android_compose_quickstart/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "live.videosdk.rtc.android.quickstart"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "live.videosdk.rtc.android.quickstart"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
